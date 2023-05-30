1b9347a67762e2c172aafdc8031172da2ded89cf
Alireza Gholami <Alireza5Gholami@gmail.com>
2023-05-30 18:51:14 +0430

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ul className="divide-y divide-gray-200">
+      {people.map((person) => (
+        <li key={person.email} className="py-4 flex">
+          <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900">{person.name}</p>
+            <p className="text-sm text-gray-500">{person.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default App
+export default App;
