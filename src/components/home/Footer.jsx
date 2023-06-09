3dbfd62d3ea19b710e758b00df8d9aa3eadd7d8a
Alireza Gholami <Alireza5Gholami@gmail.com>
2023-06-09 12:16:43 +0430

    footer added

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
 
 //image 
+import footer from "../../img/footer.svg"
 import telegram from "../../img/icons/telegram.svg"
 import instagram from "../../img/icons/instagram.svg"
 import twitter from "../../img/icons/twitter.svg"
 
 const Footer = () => {
     return (
-        <div>
-            <div>
-                <h5>دسترسی آسان</h5>
-                <ul>
+        <div 
+            style={{background : `url(${footer})`}}
+            className='flex justify-around h-[319px]'
+            > 
+            <div
+                className='flex flex-col justify-center gap-[16px] fontEstedad text-white ' 
+                >
+                <h5 className='text-xl font-bold'>دسترسی آسان</h5>
+                <ul className='flex flex-col gap-[16px]'>
                     <li>پرسش‌های متداول</li>
                     <li>قوانین ترخینه</li>
                     <li>حریم خصوصی</li>
-                    <li>
-                        <img src={telegram} alt="telegram" />
-                        <img src={instagram} alt="instagram" />
-                        <img src={twitter} alt="twitter" />
+                    <li className='flex justify-between'>
+                        <a href="#"><img src={telegram} alt="telegram" /></a>
+                        <a href="#"><img src={instagram} alt="instagram" /></a>
+                        <a href="#"><img src={twitter} alt="twitter" /></a>
                     </li>
                 </ul>
             </div>
-            <div></div>
-            <div></div>
+            <div className='flex flex-col justify-center gap-[16px] items-start fontEstedad text-white'>
+                <h5 className='text-xl font-bold'>شعبه های ترخینه</h5>
+                <ul className='flex flex-col gap-[16px]'>
+                    <li>شعبه اکباتان</li>
+                    <li>شعبه چالوس</li>
+                    <li>شعبه اقدسیه</li>
+                    <li>شعبه ونک</li>
+                </ul>
+            </div>
+            <div className='relative flex gap-[16px] flex-col justify-center fontEstedad text-white'>
+                <h5 className='text-xl font-bold'>پیام به ترخینه</h5>
+                <div className='flex'>
+                    <div className='flex flex-col ml-[26px]'>
+                        <input className='bg-transparent h-[40px] mb-[12px] rounded border-[#717171] border-[1px]' placeholder='نام و نام خانوادگی'/>
+                        <input className='bg-transparent h-[40px] mb-[12px] rounded border-[#717171] border-[1px]' placeholder='شماره تماس'/>
+                        <input className='bg-transparent h-[40px] mb-[12px] rounded border-[#717171] border-[1px]' placeholder='ایمیل (اختیاری)'/>
+                    </div>
+                    <textarea cols="30" maxLength="200" className='flex items-start h-[144px] rounded border-[#717171] border-[1px] bg-transparent' placeholder='پیام شما'/>    
+                </div>
+                <button className='absolute top-[80%] right-[62%] border w-[183px] rounded border-[#717171] p-[8px_16px]' >ارسال پیام</button>
+            </div>
         </div>
     );
 };
--- src/components/home/TarkhinehGardi.jsx
+++ src/components/home/TarkhinehGardi.jsx
@@ -8,29 +8,29 @@ import arrow from "../../img/branch-img/arrow-left.svg"
 
 const TarkhinehGardi = () => {
     return (
-        <div className='flex flex-col mt-12'>
+        <div className='flex flex-col mt-12 '>
             <div className='flex justify-center m-[15px_auto]'>
                 <span className='fontEstedad font-bold leading-9 text-2xl'>ترخینه گردی</span>
             </div>
-            <div className='flex flex-row m-[0_4vw]'>
-                <Branch gallery={gallery} group={"a"} img={image1} name={"شعبه ونک"} address={"میدان ونک، خیابان فردوسی، نبش کوچه نیلوفر، پلاک ۲۶"}/>
-                <Branch gallery={gallery} group={"b"} img={image1} name={"شعبه اقدسیه"} address={"خیابان اقدسیه ، نرسیده به میدان خیام، پلاک ۸"}/>
-                <Branch gallery={gallery} group={"c"} img={image2} name={"شعبه چالوس"} address={"چالوس، خیابان ۱۷ شهریور، بعد کوچه کوروش، جنب داروخانه دکتر میلانی"}/>
-                <Branch gallery={gallery} group={"d"} img={image2} name={"شعبه اکباتان"} address={"شهرک اکباتان، فاز ۳، مجتمع تجاری کوروش، طبقه سوم"}/>    
+            <div className='flex flex-row m-[0_4vw] h-[400px]'>
+                <Branch gallery={gallery} img={image1} name={"شعبه ونک"} address={"میدان ونک، خیابان فردوسی، نبش کوچه نیلوفر، پلاک ۲۶"}/>
+                <Branch gallery={gallery} img={image1} name={"شعبه اقدسیه"} address={"خیابان اقدسیه ، نرسیده به میدان خیام، پلاک ۸"}/>
+                <Branch gallery={gallery} img={image2} name={"شعبه چالوس"} address={"چالوس، خیابان ۱۷ شهریور، بعد کوچه کوروش، جنب داروخانه دکتر میلانی"}/>
+                <Branch gallery={gallery} img={image2} name={"شعبه اکباتان"} address={"شهرک اکباتان، فاز ۳، مجتمع تجاری کوروش، طبقه سوم"}/>    
             </div>
             </div>
     );
 };
 
-const Branch = ({img , name , address, gallery , group}) => {
+const Branch = ({img , name , address, gallery}) => {
     return (
-        <div className={`group grid m-[15px_auto] bg-white fontEstedad w-[20vw]  rounded-lg border-solid border-[#417F56] border-[1px]`}>
+        <div className={`relative group grid m-[15px_auto] bg-white fontEstedad w-[20vw] h-[360px] rounded-lg border-solid border-[#417F56] border-[1px]`}>
             <img className="" alt='picture' src={img}/>
-            <img  className='flex justify-center items-center' src={gallery} alt="picture"/>
+            <img  className='hidden group-hover:block absolute top-[25%] right-[45%]' src={gallery} alt="picture"/>
             <div className=''>
                 <h5 className='flex items-center justify-center font-bold'>{name}</h5>
                 <span className='flex mb-2 text-sm text-center items-center justify-center'>{address}</span>    
-                <a className={`hidden group-hover:flex justify-center items-center text-xs m-[0_auto] mb-4 w-[10vw] h-9 text-[#315F41] rounded-sm border-solid border-[#417F56] border-[1px]`}>صفحه شعبه <img className='w-4' src={arrow} alt='arrow' /></a>
+                <a href='#' className={`hidden group-hover:flex justify-center items-center text-xs m-[0_auto] mb-4 w-[10vw] h-9 text-[#315F41] rounded border-solid border-[#417F56] border-[1px]`}>صفحه شعبه <img className='w-4' src={arrow} alt='arrow' /></a>
             </div>
             </div>
     )
